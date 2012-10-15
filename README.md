7d771a0033e4418f8a76b2579dbb9e43e889a56e
Leonid Shevtsov <leonid@shevtsov.me>
2012-10-15 13:46:19 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,28 @@
+# Clickable URLs
+
+A plugin for [Sublime Text 2](http://sublimetext.com)
+
+![Screenshot of a clickable URL](screenshot.png)
+
+## Summary
+
+*(Technically the URLs are not clickable per se, but openable with a key combination. Close enough!)*
+
+This plugin finds URLs in files opened in Sublime Text, underlines them, and lets you open them with a command (bound by default to `Cmd+Enter`).
+
+You can either put the cursor over an URL and hit `Cmd+Enter`, or select "Open URL under cursor" from the Command Palette. Instead of selecting an
+auto detected URL, you can select any block of text and it will also open in a browser.
+
+There is also an "Open all URLs" command, which opens all URLs found in the current document.
+
+**Behavior warning.** This plugin doesn't really let you *click* on a URL, and won't, until Sublime Text provides a civilized API for handling mouse clicks.
+
+**Performance warning.** The plugin is automatically disabled if the document has more than 200 URLs, in order to avoid a massive performance hit.
+
+## Installation
+
+Drop the plugin into Sublime Text's Packages folder.
+
+* * *
+
+By [Leonid Shevtsov](http://leonid.shevtsov.me)
---
+++ clickable_urls.py
@@ -0,0 +1,76 @@
+import sublime
+import sublime_plugin
+import webbrowser
+
+
+class UrlHighlighter(sublime_plugin.EventListener):
+    # Thanks Jeff Atwood http://www.codinghorror.com/blog/2008/10/the-problem-with-urls.html
+    # ^ that up here is a URL that should be matched
+    URL_REGEX = "\\bhttps?://[-A-Za-z0-9+&@#/%?=~_()|!:,.;]*[-A-Za-z0-9+&@#/%=~_(|]"
+
+    # TODO this amount should be configurable
+    # TODO URL underlining should also be disableable from the config
+    MAX_URLS = 200
+
+    urls_for_view = {}
+    scopes_for_view = {}
+    ignored_views = []
+
+    def on_load(self, view):
+        self.update_url_highlights(view)
+
+    def on_modified(self, view):
+        self.update_url_highlights(view)
+
+    def on_close(self, view):
+        self.urls_for_view.delete(view.id())
+        self.scopes_for_view.delete(view.id())
+        self.ignored_views.delete(view.id())
+
+    def update_url_highlights(self, view):
+        if view.id() in UrlHighlighter.ignored_views:
+            return
+
+        self.remove_old_highlights(view)
+        urls = view.find_all(UrlHighlighter.URL_REGEX)
+
+        # Avoid slowdowns for views with too much URLs
+        if len(urls) > UrlHighlighter.MAX_URLS:
+            print "UrlHighlighter: ignoring view with %u URLs" % len(urls)
+            UrlHighlighter.ignored_views.append(view.id())
+            return
+
+        UrlHighlighter.urls_for_view[view.id()] = urls
+
+        # We need separate regions for each lexical scope for ST to use a proper color for the underline
+        scope_map = {}
+        for url in UrlHighlighter.urls_for_view[view.id()]:
+            scope_name = view.scope_name(url.a)
+            scope_map.setdefault(scope_name, [])
+            scope_map[scope_name] += [sublime.Region(pos, pos) for pos in range(url.a, url.b)]
+
+        for scope_name in scope_map:
+            view.add_regions(u'clickable-urls ' + scope_name, scope_map[scope_name], scope_name, sublime.DRAW_EMPTY_AS_OVERWRITE)
+        UrlHighlighter.scopes_for_view[view.id()] = scope_map.keys()
+
+    def remove_old_highlights(self, view):
+        if view.id() in self.scopes_for_view:
+            for scope_name in self.scopes_for_view[view.id()]:
+                view.erase_regions(u'clickable-urls ' + scope_name)
+
+
+class OpenUrlCommand(sublime_plugin.TextCommand):
+    def run(self, edit):
+        if self.view.id() in UrlHighlighter.urls_for_view:
+            selection = self.view.sel()[0]
+            if selection.empty():
+                selection = next(url for url in UrlHighlighter.urls_for_view[self.view.id()] if url.contains(selection))
+            url = self.view.substr(selection)
+            webbrowser.open(url)
+
+
+class OpenAllUrlsCommand(sublime_plugin.TextCommand):
+    def run(self, edit):
+        if self.view.id() in UrlHighlighter.urls_for_view:
+            for url in set([self.view.substr(url_region) for url_region in UrlHighlighter.urls_for_view[self.view.id()]]):
+                webbrowser.open(url)
